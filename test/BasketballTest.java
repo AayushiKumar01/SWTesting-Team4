2a4820bf32a41d778e0b5f7e8e6f501db4878999
AayushiKumar01 <97132349+AayushiKumar01@users.noreply.github.com>
2022-10-12 15:26:59 -0700

    Added Test file and a dummy UnitTest for main()

---
+++ test/BasketballTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Disabled;
+
+import org.junit.jupiter.api.Test;
+
+class BasketballTest {
+    @Test
+    public void Basketball_Valid_testcaseformain(){
+        String[] basketball = {"basket", "ball"};
+        Basketball.main(basketball);
+
+    }
+
+}
