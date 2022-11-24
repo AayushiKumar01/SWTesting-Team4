bfa9142f703243bb95daa637c1d050773153a480
Frank Zhang <vvqqdd@gmail.com>
2022-11-23 21:00:22 -0800

    Added test_opponent_non_jump_shot_scenario4

--- test/BasketballTest.java
+++ test/BasketballTest.java
@@ -598,6 +598,21 @@ public class BasketballTest {
     }
 
     @Test
+    public void test_opponent_non_jump_shot_scenario4()
+    {
+        beforeEachSetUp();
+        Basketball bb = Mockito.spy(new Basketball());
+        Mockito.doNothing().when(bb).opponent_non_jumpshot();
+        bb.defense = 6;
+        Mockito.when(Math.random()).thenReturn(0.5).thenReturn(0.6).thenReturn(0.75);
+        bb.opponent_non_jumpshot();
+        Assert.assertTrue(outputStreamCaptor.toString().trim().contains(""));
+        Mockito.verify(bb, Mockito.times(1)).opponent_non_jumpshot();
+        afterEachSetup();
+    }
+
+
+    @Test
     public void test_dartmouth_non_jump_shot_halftime()
     {
         beforeEachSetUp();
